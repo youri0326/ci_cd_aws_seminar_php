acc8976a853c500d8c7a9eaac228227c66a26453
Yoshiike <y.yoshiike44187@gmail.com>
2025-10-26 20:28:25 +0900

    Add initial project files for CI/CD setup

--- setup_cicd_php.sh
+++ setup_cicd_php.sh
@@ -2,6 +2,8 @@
 # PHP セットアップ
 # ------------------------------
 echo "=== PHP セットアップ開始 ==="
+export REGION=ap-northeast-1
+
 
 # ディレクトリ作成
 mkdir -p /mnt/c/ci_cd_aws_seminar_php
@@ -36,27 +38,31 @@ aws codebuild create-project \
   --artifacts type=CODEPIPELINE \
   --environment type=LINUX_CONTAINER,computeType=BUILD_GENERAL1_SMALL,image=aws/codebuild/standard:5.0 \
   --service-role arn:aws:iam::963220189927:role/CodeBuildServiceRole-yoshiike-20251019 \
-  --region ${AWS_REGION}
+  --region ${REGION}
 
 # CodeDeploy アプリ作成
 aws deploy create-application \
   --application-name cicd-aws-codedeploy-php-yoshiike-20251019 \
   --compute-platform ECS \
-  --region ${AWS_REGION}
+  --region ${REGION}
 
 # CodeDeploy デプロイグループ作成
+# aws deploy create-deployment-group \
+#   --application-name cicd-aws-codedeploy-php-yoshiike-20251019 \
+#   --deployment-group-name cicd-aws-codedeploy-php-group \
+#   --service-role-arn arn:aws:iam::963220189927:role/CodeDeployServiceRole-yoshiike-20251019 \
+#   --deployment-style deploymentType=BLUE_GREEN,deploymentOption=WITH_TRAFFIC_CONTROL \
+#   --target-group-pair-info file://CodeDeploy/tg-pair.json \
+#   --ecs-services clusterName=ecs-cluster-yoshiike-20251019,serviceName=php-service-yoshiike-20251019 \
+#   --region ${REGION}
+
 aws deploy create-deployment-group \
-  --application-name cicd-aws-codedeploy-php-yoshiike-20251019 \
-  --deployment-group-name cicd-aws-codedeploy-php-group \
-  --service-role-arn arn:aws:iam::963220189927:role/CodeDeployServiceRole-yoshiike-20251019 \
-  --deployment-style deploymentType=BLUE_GREEN,deploymentOption=WITH_TRAFFIC_CONTROL \
-  --target-group-pair-info file://CodeDeploy/tg-pair.json \
-  --ecs-services clusterName=ecs-cluster-yoshiike-20251019,serviceName=php-service-yoshiike-20251019 \
-  --region ${AWS_REGION}
+  --cli-input-json file://CodeDeploy/tg-pair.json \
+  --region ap-northeast-1
 
 # CodePipeline 作成（Blue/Green）
 aws codepipeline create-pipeline \
   --cli-input-json file://CodePipeline/pipeline-php-bluegreen.json \
-  --region ${AWS_REGION}
+  --region ${REGION}
 
 echo "=== すべてのセットアップが完了しました ==="
